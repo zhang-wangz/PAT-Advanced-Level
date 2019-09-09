3cbeaf3ba5b135e24d2689c2afd8767017c991f6
zhang-wangz <745268436@qq.com>
2019-09-09 19:25:45 +0800

    Initial Commit

---
+++ PAT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PAT
+//
+//  Created by wangz on 2019/9/9.
+//  Copyright © 2019 wangz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
