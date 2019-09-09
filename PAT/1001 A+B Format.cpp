dad85a746a10e60f81dbc43aa3344c94eeab98b9
zhang-wangz <745268436@qq.com>
2019-09-09 20:59:12 +0800

    init

--- PAT/1001 A+B Format.cpp
+++ PAT/1001 A+B Format.cpp
@@ -7,9 +7,8 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+    
 }
---
+++ PAT1002/1002 A+B for Polynomials.cpp
@@ -0,0 +1,13 @@
+//
+//  1002 A+B for Polynomials.cpp
+//  PAT
+//
+//  Created by wangz on 2019/9/9.
+//  Copyright © 2019 wangz. All rights reserved.
+//
+
+#include "iostream"
+using namespace std;
+int main(){
+    cout<<"sda";
+}
