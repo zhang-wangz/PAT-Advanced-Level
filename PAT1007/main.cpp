dbe4766d2704edf2e3e411ec9616492f6616a03d
zhang-wangz <745268436@qq.com>
2019-09-18 11:09:53 +0800

    PAT1007

--- PAT1007/main.cpp
+++ PAT1007/main.cpp
@@ -3,22 +3,28 @@
 #include <vector>
 using namespace std;
 
-vector<int>v;
+vector<int>v(n);
 int main(){
     int k;
     scanf("%d",&k);
     int x;
-    int temp=0,sum=0;
+    int temp=0,sum=-1;
+    int tempindex=0;
     int left=0,right=k-1;
     for(int i=0;i<k;i++){
         scanf("%d",&x);
         v.push_back(x);
+        temp += x;
         if(temp < 0){
             temp=0;
-            left=i+1;
+            tempindex=i+1;
         }else if(temp > sum){
             sum=temp;
-            right=i+1;
+            left=tempindex;
+            right=i;
         }
     }
+    // 注意一下全部值都是负数的情况
+    if(sum<0)sum=0;
+    printf("%d %d %d",sum,v[left],v[right]);
 }
---
+++ PAT1008/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PAT1008
+//
+//  Created by wangz on 2019/9/18.
+//  Copyright © 2019 wangz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
